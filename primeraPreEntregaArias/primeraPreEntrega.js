86e859a02ec555eb7c65d1300bb32e74ee92d7fe
Rodrigo <ezequielcomar86@gmail.com>
2023-06-05 20:45:29 -0300

    Realizacion de la primera PreEntrega

---
+++ primeraPreEntregaArias/primeraPreEntrega.js
@@ -0,0 +1,26 @@
+"use strict";
+let producto = "";
+let precio = 0;
+let ciclo = true;
+const verificarNum = () => {
+  precio = prompt("Seleccione el precio");
+  if (!Number.isNaN(precio) && precio >= 0 && precio.trim() !== "") {
+  } else {
+    alert("el valor no es numerico");
+    precio = -1;
+  }
+  return precio;
+};
+const guardarProductos = (product, price) => {
+  alert("El producto es: " + product);
+  alert("El precio es: " + price);
+};
+while (ciclo === true) {
+  producto = prompt("Seleccione el producto");
+  precio = verificarNum();
+  while (precio === -1) {
+    precio = verificarNum();
+  }
+  guardarProductos(producto, precio);
+  ciclo = confirm("Quieres volver a ingresar un producto?");
+}
